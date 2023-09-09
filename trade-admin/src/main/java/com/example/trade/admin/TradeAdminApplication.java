5bd2a31ca592179ceb1cca4be5c8e642a76eca6a
Chormun <chormunlam@yahoo.com.hk>
2023-09-09 19:08:22 -0400

    Merged unrelated histories

---
+++ trade-admin/src/main/java/com/example/trade/admin/TradeAdminApplication.java
@@ -0,0 +1,18 @@
+package com.example.trade.admin;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+@ComponentScan(basePackages = {"com.example"})
+@MapperScan("com.example.trade.coupon.db.mappers")
+
+@SpringBootApplication
+public class TradeAdminApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TradeAdminApplication.class, args);
+	}
+
+}
---
+++ trade-admin/src/test/java/com/example/trade/admin/TradeAdminApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.trade.admin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TradeAdminApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
